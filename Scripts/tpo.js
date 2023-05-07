beb2a8cd0ecdfe55b4b6e2c23dadb5baaea032aa
Angel Ramos Aleman <alramos06@hotmail.com>
2023-05-07 13:31:24 -0300

    Primer commit de rama de formulario

--- Scripts/tpo.js
+++ Scripts/tpo.js
@@ -1,1 +1,13 @@
+function validateForm(form){
+    var valid=true;
+    console.log();
+    // if($("#contactName").val()=="")
+    // {
+    //     valid=false;
+    // }
 
+    if(!valid){
+        alert("Debe completar todos los datos");
+    }
+    return valid;
+}
