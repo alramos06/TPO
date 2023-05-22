34517ad775203b167e50c353c04831c25f499884
Angel Ramos Aleman <alramos06@hotmail.com>
2023-05-21 22:35:01 -0300

    merge gastronomia

--- Scripts/tpo.js
+++ Scripts/tpo.js
@@ -2,7 +2,7 @@ let menu = document.querySelector(".navigation");
 menu.innerHTML=`<nav class="menu">
                     <ul class="menu" type="none">
                         <li class="menu item1"><a href="index.html">Historia</a></li>
-                        <li class="menu item2"><a href="gastronomia.html">Gastronomia</a></li>
+                        <li class="menu item2"><a href="gastronomia.html">Gastronomía</a></li>
                         <li class="menu item3"><a href="Entertainment.html">Entretenimiento</a></li>
                         <li class="menu item4"><a href="contact.html">Contacto</a></li>
                     </ul>
